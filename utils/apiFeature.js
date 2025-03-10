86367b6e0761fb0600c6979b0f773e3c92aa98ba
Khush <kapvm4444@gmail.com>
2025-03-10 20:29:03 +0530

    increasing query fields limit

--- utils/apiFeature.js
+++ utils/apiFeature.js
@@ -48,7 +48,7 @@ class ApiFeature {
   // 4 pagination
   paginate() {
     const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 10;
+    const limit = this.queryString.limit * 1 || 200;
     const skip = (page - 1) * limit;
 
     this.query = this.query.skip(skip).limit(limit);
